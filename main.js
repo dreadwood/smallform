df6782cd58f401a5da745bea17f104e0c53f2b51
srg <skabass@yandex.ru>
2021-06-24 20:37:20 +0300

    refactoring + add gitignore

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 const RenderPosition = {
   AFTER_BEGIN: `afterbegin`,
   BEFOR_END: `beforeend`,
 };
 
-const mock = '"inputs":[{"label":"Фамилия","type":"text","id":"last_name"},{"label":"Возраст","type":"number","id":"age"}],"submit": {"url":"www.example.com","text": "Отправить"}';
+const mock =
+  '"inputs":[{"label":"Фамилия","type":"text","id":"last_name"},{"label":"Возраст","type":"number","id":"age"}],"submit": {"url":"www.example.com","text": "Отправить"}';
 
 const createFormTemplate = (data) => {
-  return (
-    `<form 
+  return `<form 
       class=${data.class}" 
       action="${data.url}" 
       method="${data.method || null}"
@@ -17,13 +17,11 @@ const createFormTemplate = (data) => {
       enctype="${data.enctype || null}"
     >
       <button type="submit">${data.text}</button>
-    </form>`
-  )
+    </form>`;
 };
 
 const createInputTemplate = (data) => {
-  return (
-    `<label for="${data.id}">${data.label}</label>
+  return `<label for="${data.id}">${data.label}</label>
     <input 
       class=${data.class}" 
       type="${data.type}" 
@@ -31,52 +29,56 @@ const createInputTemplate = (data) => {
       name="${data.name}" 
       checked="${data.checked}" 
       autocomplete="${data.autocomplete || null}"
-      ${required ? 'required' : null}"
-    >`
-  )
+      ${required ? "required" : null}"
+    >`;
 };
 
 const createTextariaTemplate = (data) => {
-  return (
-    `<label for="${data.id}">${data.label}</label>
+  return `<label for="${data.id}">${data.label}</label>
     <textaria 
       class=${data.class}" 
       id="${data.id}" 
       name="${data.name}" 
       autocomplete="${data.autocomplete || null}"
-      ${required ? 'required' : null}"
-    ></textaria>`
-  )
+      ${required ? "required" : null}"
+    ></textaria>`;
 };
 
-const renderTemplate = (container, template, place = RenderPosition.BEFOR_END) => {
+const renderTemplate = (
+  container,
+  template,
+  place = RenderPosition.BEFOR_END
+) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const generateForm = (container, data) => { // Изменить имя
+// Изменить имя
+const generateForm = (container, data) => {
   renderTemplate(container, createFormTemplate(data.submit));
 
-  const formElement = container.querySelector('form');
+  const formElement = container.querySelector("form");
   if (data.inputs) {
-    data.inputs.forEach((input) => renderTemplate(
-      formElement, 
-      createInputTemplate(input), 
-      RenderPosition.AFTER_BEGIN
-    ));
+    data.inputs.forEach((input) =>
+      renderTemplate(
+        formElement,
+        createInputTemplate(input),
+        RenderPosition.AFTER_BEGIN
+      )
+    );
   }
-}
+};
 
-const rootElement = document.querySelector('#root');
-const exampleElement = document.querySelector('.example');
+const rootElement = document.querySelector("#root");
+const exampleElement = document.querySelector(".example");
 
 if (exampleElement) {
-  const exampleTextElement = exampleElement.querySelector('.example__text');
-  const exampleBtnElement = exampleElement.querySelector('.example__btn');
+  const exampleTextElement = exampleElement.querySelector(".example__text");
+  const exampleBtnElement = exampleElement.querySelector(".example__btn");
 
-  exampleBtnElement.addEventListener('click', () => {
+  exampleBtnElement.addEventListener("click", () => {
     const data = JSON.parse(exampleTextElement.value);
 
-    generateForm(rootElement, data)
+    generateForm(rootElement, data);
     console.log(data);
-  })
-};
+  });
+}
